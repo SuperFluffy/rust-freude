607c18ae9cb486b39a3a67691dfd252f143781b5
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2016-02-10 14:00:25 +0100

    Move tests to their own directory.

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,10 @@
 #![feature(augmented_assignments)]
 
-extern crate float_cmp;
 #[macro_use(izip)] extern crate itertools;
 extern crate ndarray;
 
-use traits::ODE;
-use steppers::{RungeKutta4,Stepper};
+pub use traits::ODE;
+pub use steppers::{Stepper, RungeKutta4};
 
 mod steppers;
 mod traits;
-
-#[test]
-fn simple_rk4() {
-    use float_cmp::ApproxEqUlps;
-    // Some generic ODE with f'(x) = a * sin x
-    struct SimpleODE {
-        a: f64,
-        x: f64,
-    }
-
-    impl ODE for SimpleODE {
-        type State = f64;
-        fn get_state(&self) -> &f64 {
-            &self.x
-        }
-
-        fn differentiate(&self, x: &f64) -> f64 {
-            self.a *  x.sin()
-        }
-
-        fn differentiate_into(&self, x: &f64, into: &mut f64) {
-            *into = self.a * x.sin();
-        }
-
-        fn update_state(&mut self, x: &f64) {
-            self.x = *x;
-        }
-    }
-
-    let result = 1.0863557235971943; // Checked by hand through numpy
-
-    let sys = SimpleODE { a: 1., x: 1. };
-
-    // FIXME
-    // Why do we need ::<f64> here?
-    // Without > multiple applicable items in scope
-    let mut rk4 = RungeKutta4::<f64>::new(Box::new(sys));
-    rk4.do_step(0.1);
-
-    assert!(result.approx_eq_ulps(rk4.system.get_state(), 2i64));
-}
---
+++ tests/lib.rs
@@ -0,0 +1,47 @@
+extern crate float_cmp;
+
+extern crate freude;
+
+use freude::{ODE,RungeKutta4,Stepper};
+
+#[test]
+fn simple_rk4() {
+    use float_cmp::ApproxEqUlps;
+    // Some generic ODE with f'(x) = a * sin x
+    struct SimpleODE {
+        a: f64,
+        x: f64,
+    }
+
+    impl ODE for SimpleODE {
+        type State = f64;
+        fn get_state(&self) -> &f64 {
+            &self.x
+        }
+
+        fn differentiate(&self, x: &f64) -> f64 {
+            self.a *  x.sin()
+        }
+
+        fn differentiate_into(&self, x: &f64, into: &mut f64) {
+            *into = self.a * x.sin();
+        }
+
+        fn update_state(&mut self, x: &f64) {
+            self.x = *x;
+        }
+    }
+
+    let result = 1.0863557235971943; // Checked by hand through numpy
+
+    let sys = SimpleODE { a: 1., x: 1. };
+
+    // FIXME
+    // Why do we need ::<f64> here?
+    // Without > multiple applicable items in scope
+    let mut rk4 = RungeKutta4::<f64>::new(Box::new(sys));
+    rk4.do_step(0.1);
+
+    assert!(result.approx_eq_ulps(rk4.system.get_state(), 2i64));
+}
+
