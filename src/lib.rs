9e4f8879851d850d83f51de06a2d0581f6216438
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2016-02-11 16:00:11 +0100

    Expose Observer trait

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #[macro_use(izip)] extern crate itertools;
 extern crate ndarray;
 
-pub use traits::ODE;
+pub use traits::{Observer,ODE};
 pub use steppers::{Stepper, RungeKutta4};
 
 mod integrators;
