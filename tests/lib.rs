a3f58ba3f40aa218b0243dbc6411128f905f7bfb
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2016-02-11 17:23:49 +0100

    Test: integrating for n steps should give the same result.

--- tests/lib.rs
+++ tests/lib.rs
@@ -91,3 +91,64 @@ fn integrator_rk4() {
     assert!(result.approx_eq_ulps(integrator.get_state(), 2i64));
 }
 
+#[test]
+fn integrator_steps_vs_range() {
+    // Explicitly running for n steps should give the same result as running
+    // for a time interval T with timesteps of size ΔT
+    use float_cmp::ApproxEqUlps;
+    // Some generic ODE with f'(x) = a * sin x
+    struct SimpleODE {
+        a: f64,
+        x: f64,
+    }
+
+    struct UselessObserver { };
+
+    impl ODE for SimpleODE {
+        type State = f64;
+        fn get_state(&self) -> &f64 {
+            &self.x
+        }
+
+        fn differentiate(&self, x: &f64) -> f64 {
+            self.a + x.sin()
+        }
+
+        fn differentiate_into(&self, x: &f64, into: &mut f64) {
+            *into = self.a + x.sin();
+        }
+
+        fn update_state(&mut self, x: &f64) {
+            self.x = *x;
+        }
+    }
+
+    impl<T: ?Sized> Observer<T> for UselessObserver {
+        fn observe(&mut self, _ode: &mut T) { }
+    }
+
+    // FIXME
+    // Implementing Clone for the ODEs, Steppers, and Integrators would significantly
+    // remove the boiler plate here.
+    let sys1 = SimpleODE { a: 1., x: 1. };
+    let sys2 = SimpleODE { a: 1., x: 1. };
+    let step1 = RungeKutta4::<f64>::new(Box::new(sys1));
+    let step2 = RungeKutta4::<f64>::new(Box::new(sys2));
+
+    let mut integrator1 = Integrator::new(Box::new(step1));
+    let mut integrator2 = Integrator::new(Box::new(step2));
+
+    let mut obs = UselessObserver {};
+
+    let n1 = 10;
+    let dt = 0.1;
+    let t0 = 0.0;
+    let tf = t0 + dt * (n1 as f64);
+
+    integrator1.integrate_n_times(n1, dt, &mut obs); 
+
+    let (_tf,_n2) = integrator2.integrate_between(t0,tf,dt, &mut obs); 
+
+    assert!(integrator1.get_state().approx_eq_ulps(integrator2.get_state(), 2i64));
+}
+
