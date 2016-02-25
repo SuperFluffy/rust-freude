9eb61a606e2bbbafe116c0ed207c550866e27b9b
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2016-02-25 13:36:46 +0100

    Allow bservations after flexible integration periods

--- src/integrators.rs
+++ src/integrators.rs
@@ -30,6 +30,32 @@ impl<T> Integrator<T> {
         (tacc, count)
     }
 
+    pub fn integrate_time_range<I: IntoIterator<Item=f64>>(&mut self,
+                             ts: I,
+                             dt: f64,
+                             obs: &mut Observer<ODE<State = T>>)
+                             -> (f64, u64) {
+        let mut count = 0;
+
+        let mut titer = ts.into_iter();
+        let mut tacc = match titer.next() {
+            Some(t) => t,
+            None => 0f64,
+        };
+
+        for t in titer {
+        // tacc+dt ensures that we don't exceed tf, and that tf - dt < t' <= tf
+            while (tacc + dt) <= t {
+                self.stepper.do_step(dt);
+                tacc += dt;
+                count += 1;
+            }
+            obs.observe(self.stepper.get_system_mut());
+        }
+
+        (tacc, count)
+    }
+
     pub fn integrate_n_steps(&mut self, n: u64, dt: f64, obs: &mut Observer<ODE<State = T>>) {
         for _i in 0..n {
             self.stepper.do_step(dt);
@@ -40,5 +66,20 @@ impl<T> Integrator<T> {
 
     pub fn get_state(&self) -> &T {
         self.stepper.get_state()
+    }
+
+    pub fn integrate_n_range<I: IntoIterator<Item=u64>>(&mut self,
+                             ns: I,
+                             dt: f64,
+                             obs: &mut Observer<ODE<State = T>>) -> u64 {
+        let mut count = 0;
+        for n in ns.into_iter() {
+            for _i in 0..n {
+                self.stepper.do_step(dt);
+            }
+            count += n;
+            obs.observe(self.stepper.get_system_mut());
+        }
+        count
     }
 }
