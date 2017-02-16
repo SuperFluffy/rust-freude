12e40c4f34c196422c182db25865a10757363ae7
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2017-02-16 13:17:36 +0100

    Test integration methods via macro

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,12 +1,12 @@
-#[macro_use] extern crate approx;
+#[macro_use]
+extern crate approx;
 
 extern crate freude;
 extern crate ndarray;
 
-use freude::{Integrator,ODE,RungeKutta4,Stepper};
-use freude::observers::NullObserver;
+use freude::ODE;
 
-// Some generic ODE with dx/dt = a * x ⇒ c * exp(a*t)2
+// Some generic ODE with dx/dt = a * x ⇒ x(t) = c * exp(a*t)
 #[derive(Clone)]
 struct SimpleODE {
     a: f64,
@@ -34,58 +34,6 @@ impl ODE for SimpleODE {
     }
 }
 
-#[test]
-fn stepper_rk4() {
-    let sys = SimpleODE { a: 1., c: 1., x: 1. };
-
-    let timestep = 0.1;
-    let exact = sys.c * f64::exp(sys.a * timestep);
-
-    let mut rk4 = RungeKutta4::new(sys, timestep);
-    rk4.do_step();
-
-    assert_relative_eq!(exact, rk4.get_state(), max_relative=timestep.powi(4));
-}
-
-#[test]
-fn integrator_rk4() {
-    let sys = SimpleODE { a: 1., c: 1., x: 1. };
-
-    let timestep = 0.1;
-    let steps = 100;
-    let total_time = timestep * steps as f64;
-
-    let exact = sys.c * f64::exp(sys.a * total_time);
-
-    let nullobs = NullObserver::new();
-    let mut integrator = Integrator::new(RungeKutta4::new(sys, timestep), nullobs);
-
-    integrator.integrate_n_steps(steps);
-
-    assert_relative_eq!(exact, integrator.stepper_ref().get_state(), max_relative=timestep.powi(4));
-}
-
-#[test]
-fn integrator_steps_vs_range() {
-    // FIXME
-    // Implementing Clone for the ODEs, Steppers, and Integrators would significantly
-    // remove the boiler plate here.
-    let sys1 = SimpleODE { a: 1., c: 1., x: 1. };
-    let sys2 = SimpleODE { a: 1., c: 1., x: 1. };
-
-    let stepsize = 0.1;
-    let steps = 10;
-    let total_time = stepsize * (steps as f64);
-
-
-    let mut integrator1 = Integrator::new(RungeKutta4::new(sys1, stepsize), NullObserver::new());
-    let mut integrator2 = Integrator::new(RungeKutta4::new(sys2, stepsize), NullObserver::new());
-    integrator1.integrate_n_steps(steps);
-
-    let (_tf,_n2) = integrator2.integrate_time(total_time);
-
-    assert_relative_eq!(integrator1.stepper_ref().get_state(), integrator2.stepper_ref().get_state());
-}
 
 #[test]
 fn test_fold() {
@@ -100,3 +48,76 @@ fn test_fold() {
 
     assert_eq!(acc, 36);
 }
+
+macro_rules! mk_stepper_tests {
+    ($stepper:ident, $error_order:expr) => {
+        #[allow(non_snake_case)]
+        mod $stepper {
+            use freude::*;
+            use freude::observers::NullObserver;
+
+            use super::SimpleODE;
+
+            #[test]
+            fn stepper() {
+                let sys = SimpleODE { a: 1., c: 1., x: 1. };
+
+                let timestep = 0.1;
+                let exact = sys.c * f64::exp(sys.a * timestep);
+
+                let mut stepper = $stepper::new(sys, timestep);
+                stepper.do_step();
+
+                assert_relative_eq!(exact, stepper.get_state(), max_relative=timestep.powi($error_order));
+            }
+
+            #[test]
+            fn integrator() {
+                let sys = SimpleODE { a: 1., c: 1., x: 1. };
+
+                let timestep = 0.1;
+                let steps = 100;
+                let total_time = timestep * steps as f64;
+
+                let exact = sys.c * f64::exp(sys.a * total_time);
+
+                let nullobs = NullObserver::new();
+                let mut integrator = Integrator::new($stepper::new(sys, timestep), nullobs);
+
+                integrator.integrate_n_steps(steps);
+
+                assert_relative_eq!(exact, integrator.stepper_ref().get_state(), max_relative=(steps as f64 * timestep.powi($error_order)));
+            }
+
+            #[test]
+            fn integrator_steps_vs_range() {
+                // FIXME
+                // Implementing Clone for the ODEs, Steppers, and Integrators would significantly
+                // remove the boiler plate here.
+                let sys1 = SimpleODE { a: 1., c: 1., x: 1. };
+                let sys2 = SimpleODE { a: 1., c: 1., x: 1. };
+
+                let stepsize = 0.1;
+                let steps = 10;
+                let total_time = stepsize * (steps as f64);
+
+
+                let mut integrator1 = Integrator::new($stepper::new(sys1, stepsize), NullObserver::new());
+                let mut integrator2 = Integrator::new($stepper::new(sys2, stepsize), NullObserver::new());
+                integrator1.integrate_n_steps(steps);
+
+                let (_tf,_n2) = integrator2.integrate_time(total_time);
+
+                assert_relative_eq!(integrator1.stepper_ref().get_state(), integrator2.stepper_ref().get_state());
+            }
+        }
+    };
+
+    ($($stepper:ident:$error_order:expr),+) => {
+        $(
+            mk_stepper_tests!($stepper, $error_order);
+        )+
+    };
+}
+
+mk_stepper_tests!(Euler:1,Heun:2,RungeKutta4:4);
