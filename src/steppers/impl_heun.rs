9181827fbd74f537004d81c42246c7fc201d32c3
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2017-04-05 00:55:35 +0200

    Rearrange steppers

--- src/steppers/impl_heun.rs
+++ src/steppers/impl_heun.rs
@@ -4,52 +4,30 @@ use ndarray::IntoNdProducer;
 use traits::ODE;
 
 use super::{
+    Heun,
     Stepper,
     ZipMarker,
 };
 
-pub struct RungeKutta4<S, T> {
-    dt: f64,
-    dt_2: f64,
-    dt_3: f64,
-    dt_6: f64,
-
-    system: S,
-    temp: T,
-
-    k1: T,
-    k2: T,
-    k3: T,
-    k4: T,
-}
-
-impl<S,T> RungeKutta4<S,T>
+impl<S,T> Heun<S,T>
     where S: ODE<State=T>,
           T: Clone,
 {
     pub fn new(mut system: S, dt: f64, state: &T) -> Self {
-        let dt_2 = dt/2.0;
-        let dt_3 = dt/3.0;
-        let dt_6 = dt/6.0;
-
         let temp = system.differentiate(&state);
         let k1 = system.differentiate(&state);
         let k2 = system.differentiate(&state);
-        let k3 = system.differentiate(&state);
-        let k4 = system.differentiate(&state);
 
-        RungeKutta4 {
+        let dt_2 = dt/2.0;
+
+        Heun {
             dt: dt,
             dt_2: dt_2,
-            dt_3: dt_3,
-            dt_6: dt_6,
             system: system,
 
             temp: temp,
             k1: k1,
             k2: k2,
-            k3: k3,
-            k4: k4,
         }
     }
 
@@ -66,19 +44,16 @@ impl<S,T> RungeKutta4<S,T>
     }
 }
 
-impl<Sy> Stepper for RungeKutta4<Sy,f64>
-    where Sy: ODE<State=f64> + 'static,
+impl<Sy> Stepper for Heun<Sy,f64>
+    where Sy: ODE<State=f64> + 'static
 {
     type System = Sy;
     type State = f64;
 
     fn do_step(&mut self, state: &mut Self::State) {
         self.system.differentiate_into(state, &mut self.k1);
-        self.system.differentiate_into(&(*state + &self.k1 * self.dt_2), &mut self.k2);
-        self.system.differentiate_into(&(*state + &self.k2 * self.dt_2), &mut self.k3);
-        self.system.differentiate_into(&(*state + &self.k3 * self.dt), &mut self.k4);
-        self.temp = *state + &(self.dt_6 * &self.k1) + &(self.dt_3 * &self.k2) +
-            &(self.dt_3 * &self.k3) + &(self.dt_6 * &self.k4);
+        self.system.differentiate_into(&(*state + &(self.dt * &self.k1)), &mut self.k2);
+        self.temp = *state + &(self.dt_2 * &self.k1) + &(self.dt_2 * &self.k2);
         self.system.update_state(state, &self.temp);
     }
 
@@ -95,7 +70,7 @@ impl<Sy> Stepper for RungeKutta4<Sy,f64>
     }
 }
 
-impl<D, S, P: ZipMarker> Stepper for RungeKutta4<S, P>
+impl<D, S, P: ZipMarker> Stepper for Heun<S, P>
     where S: ODE<State=P> + 'static,
           P: Clone,
           D: Dimension,
@@ -108,28 +83,16 @@ impl<D, S, P: ZipMarker> Stepper for RungeKutta4<S, P>
     fn do_step(&mut self, state: &mut Self::State) {
         let dt = self.dt;
         let dt_2 = self.dt_2;
-        let dt_3 = self.dt_3;
-        let dt_6 = self.dt_6;
 
         self.system.differentiate_into(state, &mut self.k1);
 
         azip!(mut t (&mut self.temp), s (&*state), k1 (&self.k1) in {
-            *t = s + dt_2 * k1
+            *t = s + dt * k1
         });
         self.system.differentiate_into(&self.temp, &mut self.k2);
 
-        azip!(mut t (&mut self.temp), s (&*state), k2 (&self.k2) in {
-            *t = s + dt_2 * k2
-        });
-        self.system.differentiate_into(&self.temp, &mut self.k3);
-
-        azip!(mut t (&mut self.temp), s (&*state), k3 (&self.k3) in {
-            *t = s + dt * k3
-        });
-        self.system.differentiate_into(&self.temp, &mut self.k4);
-
-        azip!(mut t (&mut self.temp), s (&*state), k1 (&self.k1), k2 (&self.k2), k3 (&self.k3), k4 (&self.k4) in {
-            *t = s + dt_6 * k1 + dt_3 * k2 + dt_3 * k3 + dt_6 * k4;
+        azip!(mut t (&mut self.temp), s (&*state), k1 (&self.k1), k2 (&self.k2) in {
+            *t = s + dt_2 * ( k1 + k2 )
         });
         self.system.update_state(state, &self.temp);
     }
--- src/steppers/mod.rs
+++ src/steppers/mod.rs
@@ -7,13 +7,10 @@ use ndarray::{
 
 use traits::ODE;
 
-mod euler;
-mod heun;
-mod runge_kutta_4;
+mod impl_euler;
+mod impl_heun;
+mod impl_runge_kutta_4;
 
-pub use self::euler::*;
-pub use self::heun::*;
-pub use self::runge_kutta_4::*;
 
 /// A trait defining the interface of an integration method.
 pub trait Stepper
@@ -34,3 +31,36 @@ pub trait ZipMarker {}
 
 impl<T> ZipMarker for Vec<T> {}
 impl<D: Dimension, S: Data> ZipMarker for ArrayBase<S,D> {}
+
+pub struct Euler<S, T> {
+    dt: f64,
+    system: S,
+
+    temp: T,
+}
+
+pub struct Heun<S, T> {
+    dt: f64,
+    dt_2: f64,
+
+    system: S,
+
+    temp: T,
+    k1: T,
+    k2: T,
+}
+
+pub struct RungeKutta4<S, T> {
+    dt: f64,
+    dt_2: f64,
+    dt_3: f64,
+    dt_6: f64,
+
+    system: S,
+    temp: T,
+
+    k1: T,
+    k2: T,
+    k3: T,
+    k4: T,
+}
