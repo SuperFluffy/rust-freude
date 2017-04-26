4a0f4b1721863a87b4f4c19c20b13fe9e10c289f
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2017-04-27 00:27:25 +0200

    Implement tuple impls for new Stepper trait

--- src/stepper/tuple_impls.rs
+++ src/stepper/tuple_impls.rs
@@ -9,24 +9,16 @@ use ode::*;
 
 macro_rules! impl_stepper_for_tuples {
     ( $tuple:ty ) => {
-        impl<S> Stepper for Euler<S,$tuple>
-            where S: Ode<State=$tuple> + 'static
+        impl Stepper for Euler<$tuple>
         {
-            type System = S;
             type State = $tuple;
 
-            fn do_step(&mut self, state: &mut Self::State) {
-                self.system.differentiate_into(state, &mut self.temp);
+            fn do_step<Sy>(&mut self, system: &mut Sy, state: &mut Self::State)
+                where Sy: Ode<State = Self::State>,
+            {
+                system.differentiate_into(state, &mut self.temp);
                 self.temp = *state + self.temp * <$tuple as Splat<_>>::splat(self.dt);
-                self.system.update_state(state, &self.temp);
-            }
-
-            fn system_ref(&self) -> &Self::System {
-                &self.system
-            }
-
-            fn system_mut(&mut self) -> &mut Self::System {
-                &mut self.system
+                system.update_state(state, &self.temp);
             }
 
             fn timestep(&self) -> f64 {
@@ -34,28 +26,20 @@ macro_rules! impl_stepper_for_tuples {
             }
         }
 
-        impl<S> Stepper for Heun<S,$tuple>
-            where S: Ode<State=$tuple> + 'static
+        impl Stepper for Heun<$tuple>
         {
-            type System = S;
             type State = $tuple;
 
-            fn do_step(&mut self, state: &mut Self::State) {
+            fn do_step<Sy>(&mut self, system: &mut Sy, state: &mut Self::State)
+                where Sy: Ode<State = Self::State>,
+            {
                let dt = <$tuple as Splat<_>>::splat(self.dt);
                let dt_2 = <$tuple as Splat<_>>::splat(self.dt_2);
 
-               self.system.differentiate_into(state, &mut self.k1);
-               self.system.differentiate_into(&(*state + dt * self.k1), &mut self.k2);
+               system.differentiate_into(state, &mut self.k1);
+               system.differentiate_into(&(*state + dt * self.k1), &mut self.k2);
                self.temp = *state + dt_2 * (self.k1 + self.k2);
-               self.system.update_state(state, &self.temp);
-            }
-
-            fn system_ref(&self) -> &Self::System {
-                &self.system
-            }
-
-            fn system_mut(&mut self) -> &mut Self::System {
-                &mut self.system
+               system.update_state(state, &self.temp);
             }
 
             fn timestep(&self) -> f64 {
@@ -63,33 +47,25 @@ macro_rules! impl_stepper_for_tuples {
             }
         }
 
-        impl<S> Stepper for RungeKutta4<S,$tuple>
-            where S: Ode<State=$tuple> + 'static
+        impl Stepper for RungeKutta4<$tuple>
         {
-            type System = S;
             type State = $tuple;
 
-            fn do_step(&mut self, state: &mut Self::State) {
+            fn do_step<Sy>(&mut self, system: &mut Sy, state: &mut Self::State)
+                where Sy: Ode<State = Self::State>,
+            {
                 let dt = <$tuple as Splat<_>>::splat(self.dt);
                 let dt_2 = <$tuple as Splat<_>>::splat(self.dt_2);
                 let dt_3 = <$tuple as Splat<_>>::splat(self.dt_3);
                 let dt_6 = <$tuple as Splat<_>>::splat(self.dt_6);
 
-                self.system.differentiate_into(state, &mut self.k1);
-                self.system.differentiate_into(&(*state + dt_2 * self.k1), &mut self.k2);
-                self.system.differentiate_into(&(*state + dt_2 * self.k2), &mut self.k3);
-                self.system.differentiate_into(&(*state + dt * self.k3), &mut self.k4);
+                system.differentiate_into(state, &mut self.k1);
+                system.differentiate_into(&(*state + dt_2 * self.k1), &mut self.k2);
+                system.differentiate_into(&(*state + dt_2 * self.k2), &mut self.k3);
+                system.differentiate_into(&(*state + dt * self.k3), &mut self.k4);
 
                 self.temp = *state + dt_6 * (self.k1 + self.k4)+ dt_3 * (self.k2 + self.k3);
-                self.system.update_state(state, &self.temp);
-            }
-
-            fn system_ref(&self) -> &Self::System {
-                &self.system
-            }
-
-            fn system_mut(&mut self) -> &mut Self::System {
-                &mut self.system
+                system.update_state(state, &self.temp);
             }
 
             fn timestep(&self) -> f64 {
