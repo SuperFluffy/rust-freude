411c1c2e842459ebf99ef5d60ade8427aefc72e7
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2017-05-07 20:29:20 +0200

    Update Lorenz example to recent changes

--- examples/lorenz.rs
+++ examples/lorenz.rs
@@ -2,8 +2,7 @@ extern crate freude;
 extern crate tuple;
 
 use freude::RungeKutta4;
-use freude::Integrator;
-use freude::observers::NullObserver;
+use freude::Stepper;
 use tuple::T3;
 
 fn main() {
@@ -14,7 +13,7 @@ fn main() {
     let beta = 8.0/3.0;
     let rho = 28.0;
 
-    let f: Box<Fn(T3<_,_,_>) -> T3<_,_,_>> = Box::new(
+    let mut f: Box<Fn(T3<_,_,_>) -> T3<_,_,_>> = Box::new(
         move |T3(x,y,z): T3<f64,f64,f64>| {
             T3( sigma * (y - x),
                 x * (rho - z) - y,
@@ -23,10 +22,9 @@ fn main() {
         }
     );
 
-    let state = T3(0.5,1.0,1.5);
-    let rk4 = RungeKutta4::new(f, dt, &state);
-    let mut integrator = Integrator::new(rk4, NullObserver::new(), state);
-    let (_, steps) = integrator.integrate_time(time);
+    let mut state = T3(0.5,1.0,1.5);
+    let mut rk4 = RungeKutta4::new(&state, dt);
+    let (_, steps) = rk4.integrate_time(&mut f, &mut state, time);
     println!("Integrated Lorenz systems for {} steps", steps);
-    println!("Final state: {:?}", integrator.state_ref());
+    println!("Final state: {:?}", state);
 }
